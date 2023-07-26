11e0691140b3b17b4018de75c7331531bb14b805
fernando <gomeztoledofernando14@gmail.com>
2023-07-26 10:51:26 -0500

    integrado el Fooder

--- js/carrucel.js
+++ js/carrucel.js
@@ -1,47 +1,52 @@
- // Obtener referencias a los elementos del carrusel
- const carousel = document.getElementById('myCarousel');
- const prevButton = carousel.querySelector('.carousel-control-prev');
- const nextButton = carousel.querySelector('.carousel-control-next');
+// Obtener referencias a los elementos del carrusel
+const carousel = document.getElementById('myCarousel');
+const prevButton = carousel.querySelector('.carousel-control-prev');
+const nextButton = carousel.querySelector('.carousel-control-next');
 
- // Agregar eventos de clic a los botones
- prevButton.addEventListener('click', function(event) {
-   event.preventDefault();
-   carouselPrev();
- });
+// Agregar eventos de clic a los botones del carrusel
+prevButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  carouselPrev();
+});
 
- nextButton.addEventListener('click', function(event) {
-   event.preventDefault();
-   carouselNext();
- });
+nextButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  carouselNext();
+});
 
- // Función para mover el carrusel al elemento anterior
- function carouselPrev() {
-   const activeItem = carousel.querySelector('.carousel-item.active');
-   const prevItem = activeItem.previousElementSibling || carousel.lastElementChild;
+// Función para mover el carrusel al elemento anterior
+function carouselPrev() {
+  const activeItem = carousel.querySelector('.carousel-item.active');
+  const prevItem = activeItem.previousElementSibling || carousel.lastElementChild;
 
-   activeItem.classList.remove('active');
-   prevItem.classList.add('active');
- }
+  activeItem.classList.remove('active');
+  prevItem.classList.add('active');
+}
 
- // Función para mover el carrusel al siguiente elemento
- function carouselNext() {
-   const activeItem = carousel.querySelector('.carousel-item.active');
-   const nextItem = activeItem.nextElementSibling || carousel.firstElementChild;
+// Función para mover el carrusel al siguiente elemento
+function carouselNext() {
+  const activeItem = carousel.querySelector('.carousel-item.active');
+  const nextItem = activeItem.nextElementSibling || carousel.firstElementChild;
 
-   activeItem.classList.remove('active');
-   nextItem.classList.add('active');
- }
+  activeItem.classList.remove('active');
+  nextItem.classList.add('active');
+}
 
- //color de cuadros
+// Función para crear el Footer
+function createFooter() {
+  // Código para crear el Footer (mismo código que antes)
+  // ...
 
- 
-    function changeColor(elementId, color) {
-      var box = document.getElementById(elementId);
-      box.style.color = color;
-    }
-    
-    function changeTextColor(elementId, color) {
-      var box = document.getElementById(elementId);
-      box.style.backgroundColor = color;
-    }
-  
+  document.body.appendChild(footer);
+}
+
+// Llamar a la función para crear el Footer
+createFooter();
+
+// Función para mostrar la alerta de "Hola mundo"
+function showAlert() {
+  alert('Hola mundo');
+}
+
+// Llamar a la función para mostrar la alerta de "Hola mundo" cuando la página cargue
+window.onload = showAlert;
